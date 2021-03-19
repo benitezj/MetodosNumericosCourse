41f90b48e8b0b4929069c576750c981b8fd3c0e7
Jose <jose.benitez@cern.ch>
2021-03-19 11:00:13 -0700

    fix

--- Lesson7/generate_signal.C
+++ Lesson7/generate_signal.C
@@ -1,3 +1,4 @@
+
 float H_mass = 125;
 
 float EtaFromTheta(float theta){ return -log(tan(theta/2));}
@@ -12,16 +13,20 @@ bool det_accept(float rap){
   return 1;
 }
 
+
+
 void generate_signal(int N=100){
 
-  bool apply_efficiency = 1;
+  bool apply_efficiency = 0;
 
   //signal model (Higgs production)
-  TF1 FHiggs_eta("FHiggs_eta","exp(-0.5*x*x/9)",-20,20); 
-  TF1 FHiggs_pt("FHiggs_pt","x*exp(-x/100)",0,1000);
+  TF1 FHiggs_theta("FHiggs_theta","1",0,TMath::Pi()); 
+  TF1 FHiggs_phi("FHiggs_phi","1",0,2*TMath::Pi()); 
+  TF1 FHiggs_p("FHiggs_p","x*exp(-x/500)",0,1000);
 
   //Higgs decay model (scalar decay to two photons -> uniform angle)
-  TF1 FPhoton_angle("FPhoton_angle","1",0,3.14159);
+  TF1 FPhoton_theta("FPhoton_theta","1",0,TMath::Pi()); 
+  TF1 FPhoton_phi("FPhoton_phi","1",0,2*TMath::Pi()); 
 
   //Detector
   TF1 FDetector_eff("FDetector_eff","1",0,1);//this will be used to apply detector efficiency
@@ -38,47 +43,75 @@ void generate_signal(int N=100){
   for(int e=0;e<N;e++){
     if(e%100==0)cout<<"event "<<e<<endl;
 
-    ////////////////////////
+    
+    //////////////////////////////////
+    /// Theory model
+    /////////////////////////////////
+    
     ///Generate the Higgs
-    float eta = FHiggs_eta.GetRandom();
-    float p = FHiggs_pt.GetRandom();
+    float theta = FHiggs_theta.GetRandom();
+    float phi = FHiggs_phi.GetRandom();
+    float p = FHiggs_p.GetRandom();
     TLorentzVector Higgs_p4;
-    Higgs_p4.SetPtEtaPhiM(p,eta,0,H_mass);
+    Higgs_p4.SetPx(p*sin(theta)*cos(phi));
+    Higgs_p4.SetPx(p*sin(theta)*sin(phi));
+    Higgs_p4.SetPz(p*cos(theta));
+    Higgs_p4.SetE(sqrt(H_mass*H_mass + p*p));
 
-    ///////////////////////
+    
     ///Decay of the Higgs
     //step 1: in Higgs rest frame, photons get half the Higgs mass and random angles in theta
+    float ph_theta=FPhoton_theta.GetRandom();
+    float ph_phi=FPhoton_phi.GetRandom();
     TLorentzVector Photon1_p4;
-    Photon1_p4.SetPtEtaPhiM(H_mass/2, EtaFromTheta(FPhoton_angle.GetRandom()), 0, 0);
+    Photon1_p4.SetPx((H_mass/2)*sin(ph_theta)*cos(ph_phi));
+    Photon1_p4.SetPx((H_mass/2)*sin(ph_theta)*sin(ph_phi));
+    Photon1_p4.SetPz((H_mass/2)*cos(ph_theta));
+    Photon1_p4.SetE(H_mass/2);    
     TLorentzVector Photon2_p4;
-    Photon2_p4.SetPtEtaPhiM(H_mass/2, EtaFromTheta(FPhoton_angle.GetRandom()), 0, 0);
+    Photon2_p4.SetPx(-Photon1_p4.Px());
+    Photon2_p4.SetPy(-Photon1_p4.Py());
+    Photon2_p4.SetPz(-Photon1_p4.Pz());
+    Photon2_p4.SetE(Photon1_p4.E());
     //step 2: boost along Higgs generated momentum to get photons in lab frame
     Photon1_p4.Boost(Higgs_p4.BoostVector());
     Photon2_p4.Boost(Higgs_p4.BoostVector());
 
+
+
+    
     ////////////////////////
-    //Apply detector acceptance, save event if both photons are in the detector coverage
+    /// Detector
+    //////////////////////////
+    
+    //// detector acceptance, 
     if(apply_efficiency && !det_accept(Photon1_p4.Eta())) continue;
     if(apply_efficiency && !det_accept(Photon2_p4.Eta())) continue;
 
-    ///////////////////////
-    ////apply detector response
+    
+    //// detector efficiency
     //step 1: detection efficiency, not all photons will be detected by the sensor and electronics, apply 95% sensor efficiency
     if(apply_efficiency && FDetector_eff.GetRandom()>0.95) continue; //first photon
     if(apply_efficiency && FDetector_eff.GetRandom()>0.95) continue; //second photon
     
-    //step 2: detector resolution, apply energy and angular resolution to each photon
+    //step 2: detector resolution
     float ph1_e_res = FDetector_e_res.GetRandom();
     float ph2_e_res = FDetector_e_res.GetRandom();
     float ph1_rap_res = FDetector_rap_res.GetRandom();
     float ph2_rap_res = FDetector_rap_res.GetRandom();
-	
 
-    /////////////////////
-    //// Save to data file, first the true quantities, then the reconstructed quantities
+    
+
+    
+    //////////////////////////////
+    /// Data acquisition
+    ////////////////////////////
+    
+    //// Save to data file
     of<<Higgs_p4.Pt()<<" "<<Higgs_p4.Eta();
 
-    if(Photon1_p4.Pt()>Photon2_p4.Pt()){//Because photons are identical particles we must not be able to identify ph1 vs ph2, therefore save using momentum ordering instead
+    if(Photon1_p4.Pt()>Photon2_p4.Pt()){
+      //Because photons are identical particles we must not be able to identify ph1 vs ph2, therefore save using momentum ordering instead
       of<<" "<<Photon1_p4.Pt()<<" "<<Photon1_p4.Eta()
 	<<" "<<Photon2_p4.Pt()<<" "<<Photon2_p4.Eta()
 	<<" "<<Photon1_p4.Pt()*ph1_e_res<<" "<<(Photon1_p4.Eta()+ph1_rap_res)
